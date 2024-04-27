98c578a89dc9472612e7f19b4765bd873bfffb34
GetUpEarlier <1144154299@qq.com>
2024-04-28 01:20:23 +0800

    fix(compiler): add compilation lock

--- minit/compiler/cache.py
+++ minit/compiler/cache.py
@@ -2,6 +2,7 @@ import hashlib
 import os
 import subprocess
 from typing import Dict, List
+import filelock
 
 
 def cached_execute(commands: List[str], files: Dict[str, str]) -> str:
@@ -10,16 +11,17 @@ def cached_execute(commands: List[str], files: Dict[str, str]) -> str:
     md5_truncate = 8
     md5 = hash.hexdigest()[:md5_truncate]
     cached_path = os.path.join(os.path.expanduser("~"), ".minit", "cached", md5)
-    os.makedirs(cached_path, exist_ok=True)
-    # TODO: filelock or tempfile
-    if os.path.exists(os.path.join(cached_path, ".success")):
+    lock_path = f"{cached_path}.filelock"
+    with filelock.FileLock(lock_path):
+        os.makedirs(cached_path, exist_ok=True)
+        if os.path.exists(os.path.join(cached_path, ".success")):
+            return cached_path
+        print(f"compiling on {cached_path}")
+        os.makedirs(cached_path, exist_ok=True)
+        for name, content in files.items():
+            with open(os.path.join(cached_path, name), "w+") as f:
+                f.write(content)
+        subprocess.check_call(commands, cwd=cached_path)
+        open(os.path.join(cached_path, ".success"), 'w+').close()
+        print(f"compiling completed")
         return cached_path
-    print(f"compiling on {cached_path}")
-    os.makedirs(cached_path, exist_ok=True)
-    for name, content in files.items():
-        with open(os.path.join(cached_path, name), "w+") as f:
-            f.write(content)
-    subprocess.check_call(commands, cwd=cached_path)
-    open(os.path.join(cached_path, ".success"), 'w+').close()
-    print(f"compiling completed")
-    return cached_path
